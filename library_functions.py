4d51072330c12411abdde94105a0c6126f6969e7
Audrey Warner <audrey.warner1@outlook.com>
2024-05-10 15:00:59 -0400

    started on checkout function

--- library_functions.py
+++ library_functions.py
@@ -2,6 +2,15 @@
 from prettytable import PrettyTable
 
 
+def checkout():
+    checkout_y_or_n = input('Would you like to checkout one of these books (y/n)? ')
+    if checkout_y_or_n == 'y':
+        num = input('Which number would you like? ')
+
+        if book.status ==
+        check_out()
+
+
 #Display the entire list of books. Format it nicely.
 def display_books(list):
     display= PrettyTable(['Title', "Author"])
@@ -14,15 +23,14 @@ def search_by_author(list):
     print(f'We would be happy to help you search for a book!')
     desired_author= str(input("Please enter the author's name: "))
     print(f'We have the following books that may interest you:')
-    book_options = PrettyTable(['Title', "Author"])
+    book_options = PrettyTable(['Number','Title', "Author"])
     for book in list:
         author=book.author
         if author.find(desired_author) >= 0:
-            book_options.add_row([book.title, book.author])
+            book_options.add_row([book.number, book.title, book.author])
         else:
             continue
     print(book_options)
-
 
 
 
@@ -30,11 +38,11 @@ def search_by_author(list):
 def search_title(list):
     keyword=str(input('Please enter the keyword you would like to search: '))
     print('Results are as follows:')
-    keyword_results = PrettyTable(['Title','Author'])
+    keyword_results = PrettyTable(['Number','Title','Author'])
     for book in list:
         title=book.title
         if title.find(keyword) >= 0:
-            keyword_results.add_row([title,book.author])
+            keyword_results.add_row([book.number, title ,book.author])
             # Try to print out something if no keywords are found
         else:
             continue
