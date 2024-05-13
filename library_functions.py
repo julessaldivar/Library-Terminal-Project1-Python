8ba65b88847c903e82956427843b44f7d571e407
Audrey Warner <audrey.warner1@outlook.com>
2024-05-13 12:02:19 -0400

    Integrated with superclass 'Media' and added 'add_movie' function

--- library_functions.py
+++ library_functions.py
@@ -1,69 +1,84 @@
 # Here are all the functions required to run the library
 from prettytable import PrettyTable
 from Library_class_Book import Book
-from Library_class_Book import book_list
+from Library_class_Book import Media
+from Library_class_Book import Movie
+from Library_class_Book import media_list
 
 def checkout(list):
-    checkout_y_or_n = input('Would you like to checkout one of these books (y/n)? ')
+    checkout_y_or_n = input('Would you like to checkout one of these items (y/n)? ')
     if checkout_y_or_n == 'y':
         num = input('Which number would you like? ')
-        for book in list:
-            if book.number == num:
+        for item in list:
+            if item.number == num:
                 this_one = list[int(num)-1]
                 this_one.check_out()
         if int(num) > len(list) or int(num) < 0:
-            print(f"I'm sorry that book isn't in our library.")
+            print(f"I'm sorry that item isn't in our library.")
 
-def return_a_book(list, num):
-    for book in list:
-        if book.number == num:
-            book.return_book()
+def return_an_item(list, num):
+    for item in list:
+        if item.number == num:
+            item.return_media()
             return
     else:
-        print("Book was not found in the library.")
+        print("Item was not found in the library.")
 
 # Display the entire list of books. Format it nicely.
 def display_books(list):
-    display= PrettyTable(['Number' ,'Title', "Author", 'Status'])
-    for book in list:
-       display.add_row([book.number, book.title, book.author, book.status])
+    display= PrettyTable(['Number' ,'Title', "Book Author","Movie Director", 'Status'])
+    for item in list:
+        if isinstance(item, Book):
+            display.add_row([item.number, item.title, item.author, ' ', item.status])
+        elif isinstance(item, Movie):
+            display.add_row([item.number, item.title, ' ', item.director, item.status])
     print(display)
 
-# Search for a book by author.
+# Search for a book by author. #############################################
 def search_by_author(list):
-    print(f'We would be happy to help you search for a book!')
-    desired_author= input("Please enter the author's name: ")
-    book_options = PrettyTable(['Number','Title', "Author",'Status'])
+    print(f'We would be happy to help you search!')
+    desired_author_director= input("Please enter the author/director name: ")
+
+    media_options = PrettyTable(['Number','Title', "Book Author", "Movie Director",'Status'])
     found_media = 0
 
-    for book in list:
-        author=book.author
-        if author.find(desired_author) >= 0:
-            book_options.add_row([book.number, book.title, book.author, book.status])
-            found_media += 1
-        else:
-            continue
+    for item in list:
+        if isinstance(item, Book):
+            author=item.author
+            if author.find(desired_author_director) >= 0:
+                media_options.add_row([item.number, item.title, item.author, ' ', item.status])
+                found_media += 1
+        elif isinstance(item, Movie):
+            director=item.director
+            if director.find(desired_author_director) >= 0:
+                media_options.add_row([item.number, item.title, ' ' , item.director, item.status])
+                found_media += 1
+            else:
+                continue
 
     if found_media == 0:
         print(f"I'm sorry we didn't find any media from your search.")
     else:
-        print(f'We have the following books that may interest you:')
-        print(book_options)
-        checkout(book_list)
+        print(f'We have the following items that may interest you:')
+        print(media_options)
+        checkout(media_list)
 
 
 
 
-# Search for a book by title keyword.
+# Search for a book by title keyword. ###################################
 def search_title(list):
     keyword=str(input('Please enter the keyword you would like to search: '))
-    keyword_results = PrettyTable(['Number','Title','Author', "Status"])
+    keyword_results = PrettyTable(['Number','Title','Book Author', 'Movie Director', "Status"])
     found_media = 0
 
-    for book in list:
-        title = book.title
+    for item in list:
+        title = item.title
         if title.find(keyword) >= 0:
-            keyword_results.add_row([book.number, title ,book.author, book.status])
+            if isinstance(item, Book):
+                keyword_results.add_row([item.number, title ,item.author, ' ', item.status])
+            elif isinstance(item, Movie):
+                keyword_results.add_row([item.number, title, ' ',item.director, item.status])
             found_media += 1
         else:
             continue
@@ -71,9 +86,9 @@ def search_title(list):
     if found_media == 0:
         print(f"I'm sorry we didn't find any media from your search.")
     else:
-        print(f'We have the following books that may interest you:')
+        print(f'We have the following media that may interest you:')
         print(keyword_results)
-        checkout(book_list)
+        checkout(media_list)
 
 
 def add_book(list):
@@ -81,7 +96,17 @@ def add_book(list):
 
     index = str(len(list) + 1)
     new_book_title =  input('What is the title of the new book? ')
-    new_book_author = input('What is the author of the new book? ')
+    new_book_author = input('Who is the author of the new book? ')
     new_book = Book(index, new_book_title, new_book_author)
     list.append(new_book)
     print(f"Great! You've added {new_book.title} by {new_book.author} to the library. We appreciate your donation.")
+
+def add_movie(list):
+    print(f'Excellent, you would like to add a movie to the library!')
+
+    index = str(len(list) + 1)
+    new_movie_title =  input('What is the title of the new movie? ')
+    new_movie_director = input('Who is the director of the new movie? ')
+    new_movie = Movie(index, new_movie_title, new_movie_director)
+    list.append(new_movie)
+    print(f"Great! You've added {new_movie.title} directed by {new_movie.director} to the library. We appreciate your donation.")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import datetime
-from Library_class_Book import book_list
+from Library_class_Book import media_list
 
 from library_functions import display_books
 from library_functions import search_by_author
 from library_functions import search_title
 from library_functions import checkout
-from library_functions import return_a_book
+from library_functions import return_an_item
 from library_functions import add_book
+from library_functions import add_movie
 
 print("""Hi, Welcome to the Grand Circus Library!
 Please choose an option from the below list to begin:""")
@@ -14,19 +15,20 @@ Please choose an option from the below list to begin:""")
 valid_action=0
 while valid_action == 0:            # Create a loop to stay in while the user wants to interact with the library.
 
-    print("""    1. Display all books
-    2. Search for a book by author
-    3. Search for a book by title keyword
-    4. Return a book 
-    5. Add a book to the library """)
+    print("""    1. Display all media
+    2. Search for a book by author or a movie by director
+    3. Search by title keyword
+    4. Return an item 
+    5. Add a book to the library
+    6. Add a movie to the library """)
 
     action = int(input(f'> '))
 
     if action == 1:  # Display all the books ###################################
 
-        display_books(book_list)
+        display_books(media_list)
 
-        checkout(book_list)
+        checkout(media_list)
 
         # Staying in the library loop or not.
         another = input(f'Would you like to complete another action (y/n)? ')
@@ -37,7 +39,7 @@ while valid_action == 0:            # Create a loop to stay in while the user wa
 
     elif action == 2:  # Search for a book by author #############################
 
-        search_by_author(book_list)
+        search_by_author(media_list)
 
         # Staying in the library loop or not.
         another = input(f'Would you like to complete another action (y/n)? ')
@@ -48,9 +50,7 @@ while valid_action == 0:            # Create a loop to stay in while the user wa
 
     elif action == 3:   # Search for a book by title keyword #######################
 
-        search_title(book_list)
-
-        checkout(book_list)
+        search_title(media_list)
 
         # Staying in the library loop or not.
         another = input(f'Would you like to complete another action (y/n)? ')
@@ -61,9 +61,9 @@ while valid_action == 0:            # Create a loop to stay in while the user wa
 
     elif action == 4:   # Return a book ##############################################
 
-        num_return= input(f'What is the number of the book you would like to return? ')
+        num_return= input(f'What is the number of the item you would like to return? ')
 
-        return_a_book(book_list, num_return)
+        return_an_item(media_list, num_return)
 
         # Staying in the library loop or not.
         another = input(f'Would you like to complete another action (y/n)? ')
@@ -74,7 +74,18 @@ while valid_action == 0:            # Create a loop to stay in while the user wa
 
     elif action == 5:  # Adding a book to the library ################################
 
-        add_book(book_list)
+        add_book(media_list)
+
+        # Staying in the library loop or not.
+        another = input(f'Would you like to complete another action (y/n)? ')
+        if another == 'y':
+            valid_action = 0
+        else:
+            valid_action = 1
+
+    elif action == 6:  # Adding a moive to the library ################################
+
+        add_movie(media_list)
 
         # Staying in the library loop or not.
         another = input(f'Would you like to complete another action (y/n)? ')
